faeb0b651d4e36e78a9f1a6f32f706d960268e4e
Moritz Winter <moritz.a.winter@googlemail.com>
2011-12-03 20:11:47 +0100

    added documentation

--- Middle/src/main/java/org/rhok/txt4g/ThreadRunner.java
+++ Middle/src/main/java/org/rhok/txt4g/ThreadRunner.java
@@ -2,7 +2,18 @@ package org.rhok.txt4g;
 
 import java.util.Date;
 
+/**
+ * Runs a job frequently. As opposed to a cronjob this can hold data (i.e. cache hits)
+ * and avoids gc operations. 
+ * 
+ * @author ddebray, mwinter
+ * 
+ * @param <T>
+ *            job to be run
+ */
 public class ThreadRunner<T extends MessagingJob> implements Runnable {
+	private static final int SLEEPTIME_IN_MS = 20;
+	private static final int INTERVAL_IN_MS = 3000; // TODO: externalize to properties-file
 	private Date lastJobStarted;
 	private T job;
 
@@ -14,10 +25,10 @@ public class ThreadRunner<T extends MessagingJob> implements Runnable {
 		lastJobStarted = new Date();
 		while (true) {
 			try {
-				if (lastJobStarted.getTime() > new Date().getTime()) {
+				if (lastJobStarted.getTime() + INTERVAL_IN_MS > new Date().getTime()) {
 					job.run();
 				} else {
-					Thread.sleep(20);
+					Thread.sleep(SLEEPTIME_IN_MS);
 				}
 			} catch (InterruptedException e) {
 			}
--- Middle/src/main/java/org/rohok/txt4g/model/User.java
+++ Middle/src/main/java/org/rohok/txt4g/model/User.java
@@ -1,32 +1,45 @@
 package org.rohok.txt4g.model;
 
+/**
+ * User entity class.
+ * 
+ * @author ddebray, mwinter
+ * 
+ */
 public class User {
 	private String id;
 	private String firstname;
 	private String lastname;
 	private int age;
-	
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getFirstname() {
 		return firstname;
 	}
+
 	public void setFirstname(String firstname) {
 		this.firstname = firstname;
 	}
+
 	public String getLastname() {
 		return lastname;
 	}
+
 	public void setLastname(String lastname) {
 		this.lastname = lastname;
 	}
+
 	public int getAge() {
 		return age;
 	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
