1e557e9029bad77d98274430834ef63c51c1f531
Manuel Kallenbach <manuel.kallenbach@gmail.com>
2011-12-03 18:12:48 +0100

    add sms_identifier to feeds

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -2,7 +2,9 @@ class Feed
   include Mongoid::Document
 
   field :name
+  field :sms_identifier
   has_many :news_items
 
   validates_presence_of :name
+  validates_uniqueness_of :sms_identifier, :allow_nil => true
 end
