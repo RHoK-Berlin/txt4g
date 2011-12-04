89e22498fe2a6151911eef04dc9160230092249b
Manuel Kallenbach <manuel.kallenbach@gmail.com>
2011-12-04 11:25:09 +0100

    removed Frontend

--- Frontend/app/models/news_item.rb
+++
@@ -1,8 +0,0 @@
-class NewsItem
-  include Mongoid::Document
-
-  field :text
-  belongs_to :feed
-
-  validates_presence_of :feed_id, :text
-end
--- Frontend/app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User
-  include Mongoid::Document
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-end
